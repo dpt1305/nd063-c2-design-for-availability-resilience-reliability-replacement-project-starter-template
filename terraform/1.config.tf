5383e0faf277288d4b2213e7c1b420fe9a733cf4
dpt1305 <tungplatin@gmail.com>
2024-09-08 20:58:48 +0700

    delete useless config

--- terraform/1.config.tf
+++ terraform/1.config.tf
@@ -10,11 +10,9 @@ provider "aws" {
 	alias = "az1"
   region                   = "us-east-1"
   shared_credentials_files = ["~/.aws/credentials"]
-  # profile                  = "default"
 }
 provider "aws" {
 	alias = "az2"
   region                   = "us-west-2"
   shared_credentials_files = ["~/.aws/credentials"]
-  # profile                  = "default"
 }
